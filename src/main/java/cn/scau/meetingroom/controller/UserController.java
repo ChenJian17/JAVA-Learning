c9319723eeb5c0a0390e70df2604d22ad17c0679
hst1116 <1294178282@qq.com>
2018-07-06 16:12:15 +0800

    userController逻辑功能实现

--- src/main/java/cn/scau/meetingroom/controller/UserController.java
+++ src/main/java/cn/scau/meetingroom/controller/UserController.java
@@ -1,7 +1,10 @@
 package cn.scau.meetingroom.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.scau.meetingroom.service.UserService;
@@ -11,4 +14,21 @@ import cn.scau.meetingroom.service.UserService;
 public class UserController {
 	@Autowired
 	UserService userService;
+	
+	@RequestMapping("fore_login")
+	public String list(Model model) {
+		return "fore/login";
+	}
+	
+	@RequestMapping("judge_login")
+	public String judge(Model model, String name, String password,
+			HttpServletRequest request) {
+		String radio = request.getParameter("user");
+		if(radio == "user") {
+			
+		}else {
+			
+		}
+		return "fore/error";
+	}
 }
